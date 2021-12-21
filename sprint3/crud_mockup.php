25403d87e709ed6af7944331497158e55caff1c3
2DAW06 <2DAW06@esvirgua.es>
2021-12-21 14:16:22 +0100

    inicio CSS

---
+++ sprint3/crud_mockup.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <title>CRUD MINIJUEGOS</title>
+</head>
+
+<body>
+    <header>
+        <h1>MINIJUEGOS</h1>
+        <nav>
+            <ul>
+                <li>MINIJUEGOS</li>
+                <li>NIVELES</li>
+                <li>PARTIDAS</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <aside>
+            
+        </aside>
+        <div id="central">
+    <h3></h3>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
