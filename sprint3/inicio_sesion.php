afc991a840bd31e4d0c04543aff79b5da150229a
d <dnuezsan@gmail.com>
2022-01-02 00:41:12 +0100

    inicio de sesión

--- sprint3/inicio_sesion.php
+++ sprint3/inicio_sesion.php
@@ -5,40 +5,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo_inicio.css">
     <title>CRUD MINIJUEGOS</title>
 </head>
 
 <body>
     <header>
         <h1>GESTIÓN DE MINIJUEGOS</h1>
-        <nav>
+        <!-- <nav>
             <ul>
                 <li><a href="">MINIJUEGOS</a></li>
                 <li><a href="">NIVELES</a></li>
                 <li><a href="">PARTIDAS</a></li>
             </ul>
-        </nav>
+        </nav> -->
     </header>
     <main>
-        <aside>
+        <!-- <aside>
             <p>Operaciones</p>
             <div class="botones"><a href="">Mostrar</a></div>
             <div class="botones"><a href="">Modificar</a></div>
             <div class="botones"><a href="">Insertar</a></div>
             <div class="botones"><a href="">Borrar</a></div>
-        </aside>
+        </aside> -->
         <div id="central">
-            <h3>Nombre de la tabla</h3>
-            <h3>Nombre de la operacion</h3>
+            <h3>Inicio de sesión</h3>
+            <h3>Introduce tus datos</h3>
             <form action="" method="POST">
-                <label for="">Id de la tabla</label>
-                <input class="campos" type="text">
-                <label for="">Campo 2</label>
-                <input class="campos" type="text">
-                <label for="">Campo 3</label>
-                <input class="campos" type="text">
-                <input id="boton" type="submit" value="Enviar">
+                <label for="">Nombre de usuario</label>
+                <input class="campos" name="usuario" type="text">
+                <label for="">Contraseña</label>
+                <input class="campos" name="contraseña" type="text">
+                <input id="boton" type="submit" value="Entrar">
 
             </form>
         </div>
